33a65fae2673df945473dcdc7543a9a5464c0b8d
isaac-svg <isaacsakyi009@gmail.com>
2023-02-21 06:15:40 +0000

    second day

---
+++ src/BubbleSort.ts
@@ -0,0 +1,13 @@
+export default function bubble_sort(arr: []): void {
+  for (let i = 0; i < arr.length; ++i) {
+    for (let j = 0; j < arr.length - 1 - i; ++j) {
+      // swap the values if j>j+1
+      if (arr[j] > arr[j + 1]) {
+        // swap
+        const temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+}
